208742a1311b4220545cb012e5b7b5668570e2b7
JeremyLiew <83692777+JeremyLiew@users.noreply.github.com>
2024-04-30 22:47:55 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -4,6 +4,7 @@ from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
 # Load the original dataset
 original_data = pd.read_csv('cleaned_house_data.csv')  # Change the file path to your dataset
